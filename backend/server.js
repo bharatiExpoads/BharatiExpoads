429c6a8bf9191bbee6b1245c99af03a448fcd092
bharatiExpoads <bexpoads@gmail.com>
2025-11-03 19:53:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://bharatiexpoads-1.onrender.com' , methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 const adminProfileRoutes = require('./routes/adminProfile');
